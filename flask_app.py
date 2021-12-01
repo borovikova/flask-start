cba32be1bafbea58a9e6dd0b24779033839ad919
Ekaterina Borovikova <borovikova@zoho.eu>
2021-12-01 08:29:21 +0100

    Simple API with fake data

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+wines = [
+    {"id": 1, "alcohol": 8, "quality": 10},
+    {"id": 2, "alcohol": 12, "quality": 8},
+    {"id": 3, "alcohol": 10.5, "quality": 9},
+]
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, from TechLabs!</p>"
 
-@app.route("/hello/<name>")
-def hello(name):
-    return f"<p>hello from {name}!</p>"
 
+@app.route("/api/wines/all", methods=["GET"])
+def return_all():
+    return jsonify(wines)
