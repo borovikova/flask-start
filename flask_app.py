9f895ddd9c5edf12da9bd02e950a5ba093588aef
Ekaterina Borovikova <borovikova@zoho.eu>
2021-12-04 09:20:09 +0100

    Add new route

--- flask_app.py
+++ flask_app.py
@@ -3,11 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, from TechLabs!</p>"
 
-@app.route("/hello/<name>")
-def hello(name):
-    return f"<p>hello from {name}!</p>"
 
+@app.route("/hello/")
+def hello():
+    return f"<p>hello!</p>"
